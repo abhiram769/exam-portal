78ab7b2c24cc32fd97f9d0717caac355d5c964b0
abhiram769 <abhivenkat6969@gmail.com>
2026-06-23 16:42:48 +0530

    Added amount selection

--- exam_portal/app.py
+++ exam_portal/app.py
@@ -1,10 +1,18 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
 from config import Config
 import sqlite3
 import os
+import razorpay
 
 app = Flask(__name__)
 app.config.from_object(Config)
+
+client = razorpay.Client(
+    auth=(
+        Config.RAZORPAY_KEY_ID,
+        Config.RAZORPAY_SECRET
+    )
+)
 
 
 @app.route("/")
@@ -23,21 +31,81 @@ def register():
     name = request.form["name"]
     phone = request.form["phone"]
     telegram = request.form["telegram"]
+    amount = int(request.form["amount"])
+
+    order = client.order.create({
+        "amount": amount * 100,
+        "currency": "INR"
+    })
 
     conn = sqlite3.connect("payments.db")
     cursor = conn.cursor()
 
-    cursor.execute("""
-    INSERT INTO payments
-    (name, phone, telegram, amount, status)
-    VALUES (?, ?, ?, ?, ?)
-    """,
-    (name, phone, telegram, 99, "Pending"))
+    cursor.execute(
+        """
+        INSERT INTO payments
+        (name, phone, telegram, amount, order_id, status)
+        VALUES (?, ?, ?, ?, ?, ?)
+        """,
+        (
+            name,
+            phone,
+            telegram,
+            amount,
+            order["id"],
+            "Pending"
+        )
+    )
 
     conn.commit()
     conn.close()
 
-    return render_template("success.html")
+    return render_template(
+        "checkout.html",
+        order_id=order["id"],
+        amount=amount,
+        amount_paise=amount * 100,
+        razorpay_key=Config.RAZORPAY_KEY_ID
+    )
+
+
+@app.route("/verify-payment", methods=["POST"])
+def verify_payment():
+
+    payment_id = request.form["payment_id"]
+    order_id = request.form["order_id"]
+    signature = request.form["signature"]
+
+    try:
+        client.utility.verify_payment_signature({
+            "razorpay_payment_id": payment_id,
+            "razorpay_order_id": order_id,
+            "razorpay_signature": signature
+        })
+
+        conn = sqlite3.connect("payments.db")
+        cursor = conn.cursor()
+
+        cursor.execute(
+            """
+            UPDATE payments
+            SET payment_id=?,
+                status='Paid'
+            WHERE order_id=?
+            """,
+            (
+                payment_id,
+                order_id
+            )
+        )
+
+        conn.commit()
+        conn.close()
+
+        return redirect("/success")
+
+    except Exception as e:
+        return f"Payment Verification Failed: {e}"
 
 
 @app.route("/success")
